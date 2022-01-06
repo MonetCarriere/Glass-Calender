3995386073b80576eb2bcc7be049aebbb4134c2a
Owner <owner@Owners-MacBook-Pro.local>
2022-01-05 19:19:28 -0800

    first commit

---
+++ practice.js
@@ -0,0 +1,20 @@
+// dont mind this code this shit is completely unrelated.
+
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+yeezy.ForEach(item => item.addEventListener('click',  ))
